3ae7f580fe564b70c46309b6734491e0ec76d903
Chichari2 <100235931+Chichari2@users.noreply.github.com>
2024-06-13 14:51:57 +0200

    generate card item for every animal

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,18 +13,22 @@ with open("animals_template.html", "r") as fileobj:
 
 animals_data = load_data('animals_data.json')
 
-output = ""
+output = "" # define an empty string
 for animal in animals_data:
-  output += f"Name: {animal["name"]}\n"
-  output += f"Diet: {animal["characteristics"]["diet"]}\n"
+  # append information to each string
+  output += '<li class="cards__item">'
+  output += f"Name: {animal["name"]}<br/>\n"
+  output += f"Diet: {animal["characteristics"]["diet"]}<br/>\n"
   str_locations = ','.join(animal["locations"])
-  output += f"Location: {str_locations}\n"
+  output += f"Location: {str_locations}<br/>\n"
   try:
-    output += f"Type: {animal["characteristics"]["type"]}\n"
+    output += f"Type: {animal["characteristics"]["type"]}<br/>\n"
   except:
     KeyError
-  output += "\n"
+  output += '</li>'
+  #output += "\n"
 
+print(output)
 new_data = data[-4].replace("__REPLACE_ANIMALS_INFO__", output)
 data[-4] = new_data
 
