f8d6d2ea7d074f1ba779f5606f721829fa3cb7f5
Chichari2 <100235931+Chichari2@users.noreply.github.com>
2024-06-14 14:11:48 +0200

    add colors to animal cards

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -11,23 +11,32 @@ with open("animals_template.html", "r") as fileobj:
   data = fileobj.readlines()
   #print(data[-4])
 
+
 animals_data = load_data('animals_data.json')
 
-output = ""  # define an empty string
-for animal in animals_data:
-  # append information to each string
+def serialize_animal(animal_obj):
+  output = ""  # define an empty string
   output += '<li class="cards__item">'
-  output += f'<div class="card__title">{animal["name"]}</div>'
+  output += f'<div class="card__title">{animal_obj["name"]}</div>'
   output += '<p class="card__text">'
-  output += f"<strong>Diet</strong>: {animal["characteristics"]["diet"]}<br/>\n"
-  str_locations = ','.join(animal["locations"])
+  output += f"<strong>Diet</strong>: {animal_obj["characteristics"]["diet"]}<br/>\n"
+  str_locations = ','.join(animal_obj["locations"])
   output += f"<strong>Location</strong>: {str_locations}<br/>\n"
   try:
-    output += f"<strong>Type</strong>: {animal["characteristics"]["type"]}"
+    output += f"<strong>Type</strong>: {animal_obj["characteristics"]["type"]}<br/>\n"
+  except:
+    KeyError
+  try:
+    output += f"<strong>Color</strong>: {animal_obj["characteristics"]["color"]}<br/>\n"
   except:
     KeyError
   output += '</p>'
   output += '</li>'
+  return output
+
+output = ''
+for animal_obj in animals_data:
+    output += serialize_animal(animal_obj)
 
 new_data = data[-4].replace("__REPLACE_ANIMALS_INFO__", output)
 data[-4] = new_data
