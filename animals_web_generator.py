0f090d3b999073c8736ecd5c57860331afc67710
Chichari2 <100235931+Chichari2@users.noreply.github.com>
2024-06-14 13:38:54 +0200

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -13,22 +13,22 @@ with open("animals_template.html", "r") as fileobj:
 
 animals_data = load_data('animals_data.json')
 
-output = "" # define an empty string
+output = ""  # define an empty string
 for animal in animals_data:
   # append information to each string
   output += '<li class="cards__item">'
-  output += f"Name: {animal["name"]}<br/>\n"
-  output += f"Diet: {animal["characteristics"]["diet"]}<br/>\n"
+  output += f'<div class="card__title">{animal["name"]}</div>'
+  output += '<p class="card__text">'
+  output += f"<strong>Diet</strong>: {animal["characteristics"]["diet"]}<br/>\n"
   str_locations = ','.join(animal["locations"])
-  output += f"Location: {str_locations}<br/>\n"
+  output += f"<strong>Location</strong>: {str_locations}<br/>\n"
   try:
-    output += f"Type: {animal["characteristics"]["type"]}<br/>\n"
+    output += f"<strong>Type</strong>: {animal["characteristics"]["type"]}"
   except:
     KeyError
+  output += '</p>'
   output += '</li>'
-  #output += "\n"
 
-print(output)
 new_data = data[-4].replace("__REPLACE_ANIMALS_INFO__", output)
 data[-4] = new_data
 
